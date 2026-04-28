9e0a2898cc3803b9cc2e1d0ba7cd1219a04be755
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-28 19:18:07 +0700

    Update duino_fake_arduino.h

--- duino_fake_arduino.h
+++ duino_fake_arduino.h
@@ -1,4 +1,4 @@
-// duino_fake_arduino.h – Không dùng SysTick, delay bằng NOP
+// duino_fake_arduino.h – tự động bật clock GPIO, delay NOP
 #pragma once
 #include <stdint.h>
 #include "ch32v00x.h"
@@ -26,10 +26,15 @@
 #define PC7 15
 
 static inline void pinMode(uint8_t pin, uint8_t mode) {
-    GPIO_TypeDef *port;
+    // Bật clock cho cổng tương ứng
+    if (pin < 8) {
+        RCC->APB2PCENR |= (1 << 4); // GPIOD
+    } else {
+        RCC->APB2PCENR |= (1 << 2); // GPIOC
+    }
+
+    GPIO_TypeDef *port = (pin < 8) ? GPIOD : GPIOC;
     uint8_t pinpos = pin % 8;
-    if (pin < 8) port = GPIOD;
-    else port = GPIOC;
     uint32_t cfglr = port->CFGLR;
     cfglr &= ~((uint32_t)0xF << (4 * pinpos));
     cfglr |= (mode == OUTPUT) ? (0x3 << (4 * pinpos)) : (0x8 << (4 * pinpos));
@@ -37,20 +42,15 @@ static inline void pinMode(uint8_t pin, uint8_t mode) {
 }
 
 static inline void digitalWrite(uint8_t pin, uint8_t val) {
-    GPIO_TypeDef *port;
-    uint16_t pinmask;
-    if (pin < 8) { port = GPIOD; pinmask = (1 << pin); }
-    else { port = GPIOC; pinmask = (1 << (pin - 8)); }
+    GPIO_TypeDef *port = (pin < 8) ? GPIOD : GPIOC;
+    uint16_t pinmask = (pin < 8) ? (1 << pin) : (1 << (pin - 8));
     if (val) port->BSHR = pinmask;
     else port->BCR = pinmask;
 }
 
-// ---------- DELAY AN TOÀN TUYỆT ĐỐI ----------
 static inline void delay(uint32_t ms) {
     for (uint32_t i = 0; i < ms; i++) {
-        for (volatile uint32_t j = 0; j < 16000; j++) {
-            __NOP();
-        }
+        for (volatile uint32_t j = 0; j < 16000; j++) __NOP();
     }
 }
 
