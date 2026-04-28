63cec20faef076587c98323233c307fe22b5111a
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-29 02:45:15 +0700

    Update delay.c

--- delay.c
+++ delay.c
@@ -4,3 +4,10 @@ void delay_ms(uint32_t ms) {
     for (uint32_t i = 0; i < ms; i++)
         for (volatile uint32_t j = 0; j < 16000; j++) __asm("nop");
 }
+
+// Thêm hàm này
+uint32_t millis(void) {
+    static uint32_t fake_millis = 0;
+    fake_millis += 10;
+    return fake_millis;
+}
