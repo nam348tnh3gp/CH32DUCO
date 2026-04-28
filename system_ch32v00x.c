17ff44aa0709ecc502d227e62d469545a8617d83
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-29 03:17:12 +0700

    Update system_ch32v00x.c

--- system_ch32v00x.c
+++ system_ch32v00x.c
@@ -1,13 +1,22 @@
-// system_ch32v00x.c
+// system_ch32v00x.c – 48MHz PLL
 #include "ch32v00x.h"
 
-void SystemInit(void) {
-    // Chỉ bật HSI, không dùng PLL để tránh lỗi phức tạp
-    RCC->CTLR |= RCC_HSION;                // Bật HSI
-    while (!(RCC->CTLR & RCC_HSIRDY));     // Chờ HSI sẵn sàng
+uint32_t SystemCoreClock = 48000000;
 
-    // Chọn HSI làm clock hệ thống
+void SystemInit(void) {
+    RCC->CTLR |= RCC_HSION;
+    while (!(RCC->CTLR & RCC_HSIRDY));
+
+    FLASH->ACTLR = FLASH_ACTLR_LATENCY_0;
+
+    RCC->CFGR0 &= ~RCC_PLLSRC;
+    RCC->CFGR0 |= RCC_PLLMULL6;
+    RCC->CTLR |= RCC_PLLON;
+    while (!(RCC->CTLR & RCC_PLLRDY));
+
     RCC->CFGR0 &= ~RCC_SW;
-    RCC->CFGR0 |= RCC_SW_HSI;
-    while ((RCC->CFGR0 & RCC_SWS) != RCC_SWS_HSI);
+    RCC->CFGR0 |= RCC_SW_PLL;
+    while ((RCC->CFGR0 & RCC_SWS) != RCC_SWS_PLL);
+
+    SystemCoreClock = 48000000;
 }
