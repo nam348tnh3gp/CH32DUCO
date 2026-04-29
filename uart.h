038e6c09f7da37bfdcd9f0ab74d3cd20496169e8
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-29 18:47:42 +0700

    Update uart.h

--- uart.h
+++ uart.h
@@ -1,8 +1,10 @@
 // uart.h
 #pragma once
 #include <stdint.h>
+
 void uart_init(uint32_t baud);
 void uart_putc(char c);
 void uart_puts(const char *s);
 char uart_getc(void);
-int uart_available(void);
+int  uart_available(void);
+int  uart_getc_timeout(char *c, uint32_t timeout_ms); // <<< THÊM DÒNG NÀY
