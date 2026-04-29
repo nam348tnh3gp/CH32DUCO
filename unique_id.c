0deabfde2e89ebd999c899029f953cf1ffe390e2
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-29 18:40:02 +0700

    Update unique_id.c

--- unique_id.c
+++ unique_id.c
@@ -1,7 +1,14 @@
-// unique_id.c
 #include "unique_id.h"
-void unique_id_read(uint8_t *buf) {
-    volatile uint32_t *uid = (volatile uint32_t *)0x1FFFF7E8;
-    for (int i = 0; i < 8; i++)
-        buf[i] = ((uint8_t *)uid)[i];
+#include "ch32v00x.h"
+
+void unique_id_read(uint8_t uid[8]) {
+    uint32_t *p = (uint32_t *)0x1FFFF7E8;
+    uid[0] = p[0] >> 0;
+    uid[1] = p[0] >> 8;
+    uid[2] = p[0] >> 16;
+    uid[3] = p[0] >> 24;
+    uid[4] = p[1] >> 0;
+    uid[5] = p[1] >> 8;
+    uid[6] = p[1] >> 16;
+    uid[7] = p[1] >> 24;
 }
