9295d8a3552f44bb771f06d1751db3868d1527e5
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-04-29 18:36:13 +0700

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,18 +1,32 @@
-// gpio.c
 #include "gpio.h"
-#include "ch32v00x.h"
 
-void gpio_set_mode(uint8_t pin, uint8_t mode) {
-    volatile uint32_t *cfglr = (pin < 8) ? &GPIOD->CFGLR : &GPIOC->CFGLR;
-    uint8_t pos = pin % 8;
-    *cfglr = (*cfglr & ~(0xF << (pos*4))) | (mode << (pos*4));
-}
-void gpio_write(uint8_t pin, uint8_t val) {
+static GPIO_TypeDef* get_port(uint8_t pin, uint8_t *pinpos) {
     if (pin < 8) {
-        if (val) GPIOD->BSHR = 1 << pin;
-        else GPIOD->BCR = 1 << pin;
+        *pinpos = pin; return GPIOA;
+    } else if (pin < 16) {
+        *pinpos = pin - 8; return GPIOC;
     } else {
-        if (val) GPIOC->BSHR = 1 << (pin-8);
-        else GPIOC->BCR = 1 << (pin-8);
+        *pinpos = pin - 16; return GPIOD;
     }
+}
+
+void gpio_set_mode(uint8_t pin, GPIOMode_t mode) {
+    uint8_t pos;
+    GPIO_TypeDef *port = get_port(pin, &pos);
+    uint32_t offset = (pos & 0x7) * 4;
+    uint32_t mask = (0xFu << offset);
+    port->CFGLR = (port->CFGLR & ~mask) | ((mode & 0xFu) << offset);
+}
+
+void gpio_write(uint8_t pin, uint8_t val) {
+    uint8_t pos;
+    GPIO_TypeDef *port = get_port(pin, &pos);
+    if (val) port->BSHR = (1u << pos);
+    else     port->BCR  = (1u << pos);
+}
+
+uint8_t gpio_read(uint8_t pin) {
+    uint8_t pos;
+    GPIO_TypeDef *port = get_port(pin, &pos);
+    return (port->INDR >> pos) & 1u;
 }
